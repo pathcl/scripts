7d46e7a0ba69a2cdea7d922fbf370dcc7f519b76
Luis San Martin <lsanmartinr@gmail.com>
2014-11-24 22:29:20 -0300

    Added randomness to avoid duplicate status

--- python/cosmonauta.py
+++ python/cosmonauta.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import tweepy
 import pywapi
+import random
  
 CONSUMER_KEY=""
 CONSUMER_SECRET=""
@@ -12,6 +13,7 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 weather = pywapi.get_weather_from_weather_com('CIXX0020:1')
-tweet = "@Tia_Ivonne It is " + weather['current_conditions']['text'].lower() + " and " + weather['current_conditions']['temperature'] + "C now in Santiago" + " Max " + weather["forecasts"][0]["high"] + " Min " + weather["forecasts"][0]["low"]
-#print tweet
-api.update_status(tweet)
+tweet = "@Tia_Ivonne It is " + weather['current_conditions']['text'].lower() + " and " + weather['current_conditions']['temperature'] + "C now in Santiago" + " Max " + weather["forecasts"][0]["high"] + " Min " + weather["forecasts"][0]["low"] + random.random()*100
+
+print tweet
+#api.update_status(tweet)
---
+++ python/tcport.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Measures response time on a given TCP PORT
+# example: ./tcport.py google.cl 80
+
+import sys, socket, time
+
+host = sys.argv[1]
+port = sys.argv[2]
+
+try:
+    measure = socket.getaddrinfo(host, port, socket.AF_UNSPEC, 
+                                 socket.SOCK_STREAM)
+except socket.gaierror:
+    print "getaddrinfo() error:", sys.exc_info()[1]
+    sys.exit(1)
+
+for (family, socktype, proto, canon, sockaddr) in measure:
+    addr, port = sockaddr[0:2]
+    try:
+        t1 = time.time()
+        s = socket.socket(family, socktype)
+        s.connect(sockaddr)
+        t2 = time.time()
+        s.close()
+    except socket.error:
+        print "ERROR: %s -> %s" % (sockaddr[0], sys.exc_info()[1])
+        pass
+    else:
+        print "%-40s %8.5f ms" % (sockaddr[0], (t2-t1)*1000.0)
