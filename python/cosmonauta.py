d3b0cda8fb5d906d669d658aed15d55e444b5181
Luis San Martin <lsanmartinr@gmail.com>
2014-11-24 22:41:02 -0300

    added random module
    avoid duplicate status through random module

--- python/cosmonauta.py
+++ python/cosmonauta.py
@@ -12,8 +12,8 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-weather = pywapi.get_weather_from_weather_com('CIXX0020:1')
-tweet = "@Tia_Ivonne It is " + weather['current_conditions']['text'].lower() + " and " + weather['current_conditions']['temperature'] + "C now in Santiago" + " Max " + weather["forecasts"][0]["high"] + " Min " + weather["forecasts"][0]["low"] + random.random()*100
 
-print tweet
-#api.update_status(tweet)
+random = random.randint(0,1000)
+weather = pywapi.get_weather_from_weather_com('CIXX0020:1')
+tweet = "@Tia_Ivonne It is " + weather['current_conditions']['text'].lower() + " and " + weather['current_conditions']['temperature'] + "C now in Santiago" + " Max " + weather["forecasts"][0]["high"] + " Min " + weather["forecasts"][0]["low"] + " feels like " + weather['current_conditions']['feels_like'] + " your lucky number is: " + str(random) 
+api.update_status(tweet)
