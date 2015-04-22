30081b5a94d0c7977190b369319d7d0969de2936
pathcl <lsanmartinr@gmail.com>
2015-04-21 21:14:13 -0300

    None

--- python/monitor-jitter.py
+++ python/monitor-jitter.py
@@ -7,14 +7,12 @@ try:
 except ImportError:
    raise ImportError('Please check numpy and Decimal modules')
 
-
 def main():
     data = numpy.loadtxt('file.txt')
     if Decimal(data.sum()) > 1:
         return "Jitter Lag!"
     else:
         return "Ok"
-
 
 if __name__ == '__main__':
    print main()
